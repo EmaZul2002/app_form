473f1146bd34ef3f5088fc9adee867898b63adaa
emanuel.zulian <emanuel.zulian@mat.tn.it>
2024-02-05 11:41:11 +0100

    Primo caricamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'app_form';
+export class AppComponent implements OnInit{
+  signup : FormGroup
+
+  ngOnInit() {
+    this.signup = new FormGroup({
+      nominativo : new FormControl('',[Validators.required]),
+      email : new FormControl('',[Validators.required, Validators.email])
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signup.value)
+    this.signup.reset()
+  }
 }
